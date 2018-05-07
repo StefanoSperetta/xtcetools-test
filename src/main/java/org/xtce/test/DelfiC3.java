b55b8f193167e970f29096fde80657070437bfe7
Stefano Speretta <s.speretta@tudelft.nl>
2018-05-07 10:32:49 +0200

    Added verification if the parameter is within the nominal range

--- src/main/java/org/xtce/test/DelfiC3.java
+++ src/main/java/org/xtce/test/DelfiC3.java
@@ -8,7 +8,9 @@ import org.xtce.toolkit.XTCEContainerContentModel;
 import org.xtce.toolkit.XTCEContainerEntryValue;
 import org.xtce.toolkit.XTCEDatabase;
 import org.xtce.toolkit.XTCEDatabaseException;
+import org.xtce.toolkit.XTCEParameter;
 import org.xtce.toolkit.XTCETMStream;
+import org.xtce.toolkit.XTCEValidRange;
 
 /**
  * Example Delfi-C3 XTCE telemetry extractor
@@ -23,22 +25,28 @@ public class DelfiC3
         byte[] hk = new byte[]{(byte) 0xA8, (byte) 0x98, (byte) 0x9A, (byte) 0x40, (byte) 0x40, (byte) 0x40,
             (byte) 0x00, (byte) 0x88, (byte) 0x98, (byte) 0x8C, (byte) 0x92, (byte) 0x86,
             (byte) 0x66, (byte) 0x01, (byte) 0x03, (byte) 0xF0, (byte) 0xE1, (byte) 0x08,
-            (byte) 0xFA, (byte) 0x01, (byte) 0xDE, (byte) 0x84, (byte) 0xF4, (byte) 0xFF,
+            (byte) 0xFA, (byte) 0x01, 
+            (byte) 0xDE, (byte) 0x84, (byte) 0xF4, (byte) 0xFF,
             (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
             (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
             (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0xFF, (byte) 0x3F,
             (byte) 0x97, (byte) 0x96, (byte) 0x55, (byte) 0x00, (byte) 0x00, (byte) 0x1F,
-            (byte) 0xB6, (byte) 0xC0, (byte) 0x00, (byte) 0x20, (byte) 0x02, (byte) 0x00,
-            (byte) 0x7E, (byte) 0x3C, (byte) 0x76, (byte) 0x07, (byte) 0x00, (byte) 0xD5,
-            (byte) 0x00, (byte) 0x80, (byte) 0x02, (byte) 0x00, (byte) 0xD4, (byte) 0x03,
+            (byte) 0xB6, 
+            (byte) 0xC0, (byte) 0x00, (byte) 0x20, (byte) 0x02, (byte) 0x00,
+            (byte) 0x7E, (byte) 0x3C, (byte) 0x76, (byte) 0x07, (byte) 0x00, 
+            (byte) 0xD5, (byte) 0x00, (byte) 0x80, (byte) 0x02, (byte) 0x00, (byte) 0xD4, (byte) 0x03,
             (byte) 0x40, (byte) 0x18, (byte) 0x00, (byte) 0x90, (byte) 0x01, (byte) 0x00,
+            
             (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
             (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
             (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+            (byte) 0x00, (byte) 0x00, (byte) 0x00, 
+            
+            (byte) 0x00, (byte) 0x00, (byte) 0x00,
             (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
             (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
             (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
-            (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+            
             (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x5E, (byte) 0x7F,
             (byte) 0x0A, (byte) 0x8D};
 
@@ -79,8 +87,8 @@ public class DelfiC3
             }
 
             XTCETMStream stream = db_.getStream( "TLM" );
-
-            processFrame(stream, p);
+            
+            //processFrame(stream, p);
             processFrame(stream, hk);
 
         } catch (XTCEDatabaseException ex) 
@@ -107,9 +115,18 @@ public class DelfiC3
                 System.out.println();
             } else 
             {
-                System.out.println(": " + val.getCalibratedValue() + " "
+                System.out.print(": " + val.getCalibratedValue() + " "
                         + entry.getParameter().getUnits() + " ("
                         + val.getRawValueHex()+ ")");
+
+                if (!isWithinValidRange(entry))
+                {
+                    System.out.println(" INVALID!");
+                }
+                else
+                {
+                    System.out.println();
+                }
             }
         }
         List<String> warnings = model.getWarnings();
@@ -120,4 +137,43 @@ public class DelfiC3
         }
         System.out.println();
     }
+    
+    static private boolean isWithinValidRange(XTCEContainerContentEntry entry)
+    {
+        XTCEParameter param = entry.getParameter();
+        XTCEValidRange range = param.getValidRange();
+        if (!range.isValidRangeApplied()) {
+            return true;
+        } else {
+            String valLow =  range.isLowValueCalibrated() ? 
+                    entry.getValue().getCalibratedValue() : 
+                    entry.getValue().getUncalibratedValue();
+
+            if (range.isLowValueInclusive()) {
+                if (Double.parseDouble(valLow) < Double.parseDouble(range.getLowValue())) {
+                    return false;
+                }
+            } else {
+                if (Double.parseDouble(valLow) <= Double.parseDouble(range.getLowValue())) {
+                    return false;
+                }
+            }
+            
+            String valHigh =  range.isHighValueCalibrated() ? 
+                    entry.getValue().getCalibratedValue() : 
+                    entry.getValue().getUncalibratedValue();
+            
+            if (range.isHighValueInclusive()) {
+                if (Double.parseDouble(valHigh) > Double.parseDouble(range.getHighValue())) {
+                    return false;
+                }
+            } else {
+                if (Double.parseDouble(valHigh) >= Double.parseDouble(range.getHighValue())) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
 }
